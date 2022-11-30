a1da2287719a95dfac5acc2c56d65eb939ef9202
AlanGonçal <dev.alancarlos@gmail.com>
2022-11-30 20:57:18 -0300

    Avançando com rotas Routes

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,12 @@
 <h3>Contato (View)</h3>
+
+
+<header id="menu">
+    <nav class="menu_header">
+        <ul>
+            <li><a href="{{route('site.index')}}">Principal</a></li>
+            <li><a href="{{route('site.sobrenos')}}">Sobre-nos</a></li>
+            <li><a href="{{route('site.contato')}}">Contato</a></li>
+        </ul>
+    </nav>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,73 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','PrincipalController@principal');
-
-Route::get('sobre-nos','SobreNosController@sobreNos');
-
-Route::get('contato','ContatoController@contato');
 
 
 
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+
+// Parte privada
+
+Route::prefix('/app')->group(function () {
+
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+// Rota de redirecionamento
+Route::get('/rota1', function () {
+    echo'Rota1';
+})->name('site.rota1');
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2','rota1');
+
+Route::fallback(function(){
+ echo"A rota acessada não existe.<a href=".route('site.index')."> Clique aqui</a> para ir a página inicial!";
+});
 
 
 
 
+
+
+
+
+
+
+
+
+//Pode se passar: /contato/{nome}/{categoria}/{assunto}/{mensagem}
+// Route::get(
+//     '/contato/{nome}/{categoria_id}',
+//     function (
+//         string $nome,
+//         int $categoria_id =1
+//     ) {
+//         echo "Estamos aqui! . $nome - $categoria_id";
+//     }
+// )->where('categori_id','[0-9]+')->where('nome','[A-Z]+');
 
 /*
 * verbo:
